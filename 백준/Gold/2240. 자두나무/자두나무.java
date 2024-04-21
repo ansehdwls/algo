63457826dfbe048ab209d03415cdc4574a258cd3
ansehdwls <70702693+ansehdwls@users.noreply.github.com>
2024-04-21 20:37:41 +0900

    [Gold V] Title: 자두나무, Time: 120 ms, Memory: 14484 KB -BaekjoonHub

--- 백준/Gold/2240. 자두나무/자두나무.java
+++ 백준/Gold/2240. 자두나무/자두나무.java
@@ -1,62 +1,55 @@
-import java.util.*;
-import java.io.*;
+import java.awt.Point;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int T, W; // T: 시간, W: 이동 횟수
-    static int[] plum;
+	static int T, W ;
+	static StringTokenizer st;
+	
+	static int num[];
+	public static void main(String[] args) throws IOException {
 
-    static int[][][] dp;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        T = Integer.parseInt(st.nextToken());
-        W = Integer.parseInt(st.nextToken());
-        plum = new int[T];
+		st = new StringTokenizer(br.readLine());
+		T = Integer.parseInt(st.nextToken());
+		W = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < T; i++) {
-            plum[i] = Integer.parseInt(br.readLine());
-        }
-        dp = new int[T][W + 1][3];
+		num = new int[T];
+		for (int i = 0; i < T; i++) {
+			num[i] = Integer.parseInt(br.readLine());
+		}
 
-        if (plum[0] == 1) {
-            dp[0][0][1]++;
-        } else {
-            if (W >= 1)
-                dp[0][1][2]++;
-        }
+		int[][][] dp = new int[T + 1][W + 2][3];
+		
+		int max = 0;
 
-        for (int i = 1; i < T; i++) {
-            if (plum[i] == 1) {
-                for (int j = 0; j <= W && j <= i + 1; j++) {
-                    dp[i][j][1] = dp[i - 1][j][1] + 1;
-                    dp[i][j][2] = dp[i - 1][j][2];
-                }
+		for (int t = 1; t <= T; t++) {
+			for (int w = 1; w <= W+1; w++) {
+				 if (num[t-1] == 1) {
+	                    dp[t][w][1] = Math.max(dp[t - 1][w][1], dp[t - 1][w - 1][2]) + 1;
+	                    dp[t][w][2] = Math.max(dp[t - 1][w][2], dp[t - 1][w - 1][1]);
+	                } else {
+	                    if (t == 1 && w == 1) continue;
+	                    dp[t][w][1] = Math.max(dp[t - 1][w][1], dp[t - 1][w - 1][2]);
+	                    dp[t][w][2] = Math.max(dp[t - 1][w][2], dp[t - 1][w - 1][1]) + 1;
+	                }
+				 max = Math.max(max, Math.max(dp[t][w][1], dp[t][w][2]));
+			}
+			
+		}
 
-                for (int j = 1; j <= W; j++) {
-                    dp[i][j][1] = Math.max(dp[i - 1][j - 1][2] + 1, dp[i][j][1]);
-                    dp[i][j][2] = Math.max(dp[i - 1][j - 1][1], dp[i][j][2]);
-                }
-            } else {
-                for (int j = 0; j <= W && j <= i + 1; j++) {
-                    dp[i][j][1] = dp[i - 1][j][1];
-                    dp[i][j][2] = dp[i - 1][j][2] + 1;
-                }
-
-                for (int j = 1; j <= W; j++) {
-                    dp[i][j][1] = Math.max(dp[i - 1][j - 1][2], dp[i][j][1]);
-                    dp[i][j][2] = Math.max(dp[i - 1][j - 1][1] + 1, dp[i][j][2]);
-                }
-            }
-        }
-
-        // 최대 이동횟수를 사용하지 않은것이 정답일 수도 있나?
-        int max = 0;
-        for (int i = 0; i <= W; i++) {
-            max = Math.max(max, dp[T - 1][i][1]);
-            max = Math.max(max, dp[T - 1][i][2]);
-        }
-        System.out.println(max);
-        br.close();
-    }
+		
+		System.out.println(max);
+	}
 }
