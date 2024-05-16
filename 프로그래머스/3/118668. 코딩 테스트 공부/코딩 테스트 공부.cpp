f267c8a0c95411ab5962245bf1691979e8509837
ansehdwls <70702693+ansehdwls@users.noreply.github.com>
2024-05-16 12:44:38 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 0.22 ms, Memory: 3.8 MB -BaekjoonHub

--- 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.cpp
+++ 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.cpp
@@ -1,32 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int ans = 1e6;
 int malp = 0, mcop = 0;
-vector<vector<int>> prob;
 int dp[151][151]; // [alp][cop] = cost
 
-int dfs(int a, int c) {
-    if(a >= malp && c >= mcop) return 0;
-    if(dp[a][c] != 1e9) return dp[a][c];
-    
-     // 1증가
-    if(a < 150)
-        dp[a][c] = dfs(a + 1, c) + 1;
-    if(c < 150)
-        dp[a][c] = min(dp[a][c], dfs(a, c + 1) + 1);
-                
-    for(int i = 0; i < prob.size(); i++) {
-        int ap = prob[i][0], cp = prob[i][1], ad = prob[i][2], cd = prob[i][3], cost = prob[i][4];
-        
-        if(a >= ap && c >= cp)
-            dp[a][c] = min(dp[a][c], dfs((a + ad >= 150) ? 150 : a + ad, (c + cd) >= 150 ? 150 : c + cd) + cost);
-    }
-    
-    return dp[a][c];
-}
-
-int solution(int alp, int cop, vector<vector<int>> probl) {
-    prob = probl;
+int solution(int alp, int cop, vector<vector<int>> prob) {
     
     for(int i = 0; i < prob.size(); i++) {
         int ap = prob[i][0], cp = prob[i][1]; 
@@ -37,9 +16,33 @@ int solution(int alp, int cop, vector<vector<int>> probl) {
     
     sort(prob.begin(), prob.end());
         
-    fill(&dp[0][0], &dp[150][151], 1e9);
-        
-    int ans = dfs(alp, cop);
+    fill(&dp[0][0], &dp[150][151], 1e6);
+    
+    dp[alp][cop] = 0;
+
+    for(int j = alp; j <= 150; j++) {
+        for(int k = cop; k <= 150; k++) {
+            for(int i = 0; i < prob.size(); i++) {
+                int ap = prob[i][0], cp = prob[i][1], ad = prob[i][2], cd = prob[i][3], cost = prob[i][4]; 
+                // 1증가
+                if(k - 1 >= 0)
+                    dp[j][k] = min(dp[j][k], dp[j][k - 1] + 1);
+                if(j - 1 >= 0)
+                    dp[j][k] = min(dp[j][k], dp[j - 1][k] + 1);
+                
+                int nx = j + ad > 150 ? 150 : j + ad;
+                int ny = k + cd > 150 ? 150 : k + cd;
+                
+                if(j >= ap && k >= cp)
+                    dp[nx][ny] = min(dp[nx][ny], dp[j][k] + cost);
+            }
+        }
+    }
+    
+    for(int i = malp; i <= 150; i++) {
+        for(int j = mcop; j <= 150; j++)
+            ans = min(ans, dp[i][j]);
+    }
     
     return ans;
 }
