d9b610bd527474559ecf443bf9beb6af424b32ca
ansehdwls <70702693+ansehdwls@users.noreply.github.com>
2024-09-06 23:44:24 +0900

    [Silver II] Title: 연속합, Time: 224 ms, Memory: 23832 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -1,60 +1,46 @@
-import java.util.Scanner;
+import java.awt.Point;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int num[];
-    static int cache[];
-    public static void main(String ars[]){
-
-        Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-        num = new int [n];
-
-        int j = 0;
-        for(int i = 0 ; i < n ; i++) 
-        {
-            int a = sc.nextInt();
-            
-            if(i == 0) {
-                num[j] = a;
-                j++;
-            }
-            else{
-                if(num[j-1] < 0) 
-                {
-                        num[j] = a;
-                        j++;
-                }
-                else
-                {
-                    if(a >= 0) num[j-1] += a;
-                    else{
-                        num[j] = a;
-                        j++;
-                    }
-                }
-            }
-        }
-        cache = new int [n];
-        for(int i = 0 ; i < n ; i++) cache[i] = -1001; 
-
-        cache[0] = num[0];
-        maxNum(n-1);
-
-        int max = -1001;
-        for(int i = 0 ; i < n; i++){
-            max = Math.max(max, cache[i]);
-        }
-        
-        System.out.println(max);
-    }
-
-    public static int maxNum(int n)
-    {
-        if(cache[n] != -1001 );
-        else {
-            cache[n] = Math.max(maxNum(n-1) + num[n], num[n]);
-        }
-        return cache[n];
-    }
+	static int N;
+	static int M;
+	
+	static int dp[];
+	static int num[];
+	static StringTokenizer st;
+	static int max = -1000000001;
+	static Queue<Point> l = new LinkedList<Point>();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		dp= new int[N+1];
+		num = new int [N];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0 ; i< N; i++) {
+			num[i] = Integer.parseInt(st.nextToken());
+		}
+		dp[0] = num[0];
+		
+		for(int i = 1 ; i< N; i++) {
+			dp[i] = Math.max(num[i], dp[i-1] + num[i]);
+		}
+		for(int i = 0 ; i < N; i++) {
+			max = Math.max(max, dp[i]);
+		}
+		System.out.println(max);
+	}
+	
 }
